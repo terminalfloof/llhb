04880aca4a7f1fb22b94505b60da0240adf654df
Restyled.io <commits@restyled.io>
2020-10-02 03:25:05 +0000

    Restyled by clang-format

--- public/script.js
+++ public/script.js
@@ -1,15 +1,12 @@
-//setup socket.io
+// setup socket.io
 var socket = io();
 
 console.log(
-  "%chey there buster, welcome to the big boy dev console. type in debug to see the debug tool.",
-  `font-size: 1em; color: lime;`
-);
+    "%chey there buster, welcome to the big boy dev console. type in debug to see the debug tool.",
+    `font-size: 1em; color: lime;`);
 
-//on document ready, hide "#app".
-$(document).ready(() => {
-  $("#app").hide();
-});
+// on document ready, hide "#app".
+$(document).ready(() => { $("#app").hide(); });
 
 class DebugTool {
   constructor() {
@@ -17,19 +14,13 @@ class DebugTool {
     this._timerGlitch = false;
   }
 
-  get textGlitch() {
-    return this._textGlitch;
-  }
+  get textGlitch() { return this._textGlitch; }
 
-  get timerGlitch() {
-    return this._timerGlitch;
-  }
+  get timerGlitch() { return this._timerGlitch; }
 
   openApp() {
     $("form").hide();
-    $("#app").show(() => {
-      console.log("app opened.");
-    });
+    $("#app").show(() => { console.log("app opened."); });
   }
 
   toggleTextGlitch() {
@@ -52,30 +43,20 @@ class DebugTool {
     }
   }
 
-  stopTimer() {
-    throw new Error("The timer hasn't started yet.");
-  }
+  stopTimer() { throw new Error("The timer hasn't started yet."); }
 }
 
 var debug = new DebugTool();
 
-function glitch(
-  jQueryElement,
-  message,
-  amountOfSteps,
-  timeBetweenSteps,
-  glitchChars
-) {
+function glitch(jQueryElement, message, amountOfSteps, timeBetweenSteps,
+                glitchChars) {
   var aOS = amountOfSteps || 25;
   var tBS = timeBetweenSteps || 15;
   var gC = glitchChars || `qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()`;
   var doDebug = debug.timerGlitch;
   if (doDebug) {
-    console.log(
-      `Doing ${aOS} steps with ${tBS} seconds in between, totalling ${(aOS *
-        tBS) /
-        1000} seconds.`
-    );
+    console.log(`Doing ${aOS} steps with ${tBS} seconds in between, totalling ${
+        (aOS * tBS) / 1000} seconds.`);
   }
   let messageLength = message.length;
   let timeoutTime = 0;
@@ -86,7 +67,7 @@ function glitch(
         let glitchMessage = "";
         for (var j = 0; j < messageLength; j++) {
           glitchMessage +=
-            glitchLetters[Math.floor(Math.random() * glitchLetters.length)];
+              glitchLetters[Math.floor(Math.random() * glitchLetters.length)];
         }
         if (doDebug) {
           console.log(`set message to ${glitchMessage}.`);
@@ -105,10 +86,8 @@ function glitch(
   }
 }
 
-var stopTimer = () => {
-  throw new Error("The timer hasn't started yet.");
-};
-//just a countdown
+var stopTimer = () => { throw new Error("The timer hasn't started yet."); };
+// just a countdown
 function showEnd(date, endMessage) {
   var doDebug = debug.timerGlitch;
   var previous0s = [];
@@ -140,11 +119,14 @@ function showEnd(date, endMessage) {
     seconds = seconds.toString().padStart(2, "0");
 
     if (days == 0) {
-      glitchHtml = `<span class="glitch" style="font-size: 1em" data-text="00.">00.</span>`;
+      glitchHtml =
+          `<span class="glitch" style="font-size: 1em" data-text="00.">00.</span>`;
       if (hours == 0) {
-        glitchHtml = `<span class="glitch" style="font-size: 1em" data-text="00.00">00.00</span>`;
+        glitchHtml =
+            `<span class="glitch" style="font-size: 1em" data-text="00.00">00.00</span>`;
         if (minutes == 0) {
-          glitchHtml = `<span class="glitch" style="font-size: 1em" data-text="00.00.00">00.00.00</span>`;
+          glitchHtml =
+              `<span class="glitch" style="font-size: 1em" data-text="00.00.00">00.00.00</span>`;
           if (seconds == 0) {
             glitchHtml = ``;
           } else {
@@ -165,11 +147,11 @@ function showEnd(date, endMessage) {
     }
     $("#time").html(html);
     let debugObj = {
-      now: now,
-      distance: distance,
-      time: [days, hours, minutes, seconds],
-      glitchHTML: glitchHtml,
-      html: html
+      now : now,
+      distance : distance,
+      time : [ days, hours, minutes, seconds ],
+      glitchHTML : glitchHtml,
+      html : html
     };
     if (doDebug) {
       console.log(debugObj);
@@ -191,82 +173,74 @@ function loadStatus(status, userData) {
     // save the #app div as a variable.
     // switch the type in the status.
     switch (status.type) {
-      case "message":
-        app.html(status.data.message);
-        break;
-      case "none":
-        app.css("font-size", "2em");
-        app.html("Nothing's going on.");
-        break;
-      case "countdown":
-        app.html(
-          `<div id="glitchTime" style="display: inline;"></div><span id="time"></span>`
-        );
-        app.css("font-size", "3em");
-        console.log(status.data.time);
-        showEnd(status.data.time, status.data.end);
-        break;
-      case "survey":
-        if (
-          status.data.users.includes(userData.key) ||
-          status.data.users.includes("all")
-        ) {
-          app.css("font-size", "1em");
-          app.html(`${status.data.question}<br>`);
-          app.append(
-            `<form name="survey" id="survey"><input id="surveyResponse" name="response"></form>`
-          );
-          $("#surveyResponse").css("font-size", "1em");
-          $("#survey").submit(e => {
-            e.preventDefault();
+    case "message":
+      app.html(status.data.message);
+      break;
+    case "none":
+      app.css("font-size", "2em");
+      app.html("Nothing's going on.");
+      break;
+    case "countdown":
+      app.html(
+          `<div id="glitchTime" style="display: inline;"></div><span id="time"></span>`);
+      app.css("font-size", "3em");
+      console.log(status.data.time);
+      showEnd(status.data.time, status.data.end);
+      break;
+    case "survey":
+      if (status.data.users.includes(userData.key) ||
+          status.data.users.includes("all")) {
+        app.css("font-size", "1em");
+        app.html(`${status.data.question}<br>`);
+        app.append(
+            `<form name="survey" id="survey"><input id="surveyResponse" name="response"></form>`);
+        $("#surveyResponse").css("font-size", "1em");
+        $("#survey").submit(e => {
+          e.preventDefault();
+          let acceptedValues = status.data.allowedResponses;
+          console.log(document.forms["survey"]["response"]);
+          if (acceptedValues.includes(
+                  parseInt(document.forms["survey"]["response"].value))) {
+            socket.emit("surveyReply", userData.key,
+                        parseInt(document.forms["survey"]["response"].value));
+            $("#app").fadeOut(500, () => {
+              $("#app").html(`Thanks for submitting, ${
+                  userData
+                      .name}.<br><span style="font-size: 0.75em">You submitted <strong>${
+                  document.forms["survey"]["response"]
+                      .value}</strong></span>.`);
+              $("#app").css("font-size", "1.5em");
+              $("#app").fadeIn(500);
+            });
+          } else {
             let acceptedValues = status.data.allowedResponses;
-            console.log(document.forms["survey"]["response"]);
-            if (
-              acceptedValues.includes(
-                parseInt(document.forms["survey"]["response"].value)
-              )
-            ) {
-              socket.emit(
-                "surveyReply",
-                userData.key,
-                parseInt(document.forms["survey"]["response"].value)
-              );
-              $("#app").fadeOut(500, () => {
-                $("#app").html(
-                  `Thanks for submitting, ${userData.name}.<br><span style="font-size: 0.75em">You submitted <strong>${document.forms["survey"]["response"].value}</strong></span>.`
-                );
-                $("#app").css("font-size", "1.5em");
-                $("#app").fadeIn(500);
-              });
-            } else {
-              let acceptedValues = status.data.allowedResponses;
-              let list = "Invalid response. Please input one of these values: ";
-              for (var i = 0; i < acceptedValues.length; i++) {
-                if (i === acceptedValues.length) {
-                  list += ", " + acceptedValues[i] + ".";
-                } else if (i === 0) {
-                  list += acceptedValues[i];
-                } else {
-                  list += ", " + acceptedValues[i];
-                }
+            let list = "Invalid response. Please input one of these values: ";
+            for (var i = 0; i < acceptedValues.length; i++) {
+              if (i === acceptedValues.length) {
+                list += ", " + acceptedValues[i] + ".";
+              } else if (i === 0) {
+                list += acceptedValues[i];
+              } else {
+                list += ", " + acceptedValues[i];
               }
-              alert(list);
-              document.forms["survey"]["response"].value = "";
             }
-          });
-        } else {
-          $("#app").css("font-size", "1em");
-          $("#app").html(status.data.altMessage);
-        }
-        break;
-      default:
-        throw new Error(`Invalid status type, got "${status.type}".`);
+            alert(list);
+            document.forms["survey"]["response"].value = "";
+          }
+        });
+      } else {
+        $("#app").css("font-size", "1em");
+        $("#app").html(status.data.altMessage);
+      }
+      break;
+    default:
+      throw new Error(`Invalid status type, got "${status.type}".`);
     }
   } catch (err) {
     console.log(err);
     app.html(
-      `Looks like the developer of this site sucks at coding. There was an error. <br><br><span style="color: green; font-family: monospace;">${err.message}</span>`
-    );
+        `Looks like the developer of this site sucks at coding. There was an error. <br><br><span style="color: green; font-family: monospace;">${
+            err.message}</span>`);
   }
 }
 
@@ -274,53 +248,49 @@ $("#idinput").submit(event => {
   event.preventDefault();
   console.log(`inputted ${document.forms["idinput"]["id"].value}`);
   socket.emit(
-    "login",
-    parseInt(document.forms["idinput"]["id"].value),
-    function(result, data, status) {
-      if (result) {
-        console.log("passed.");
-        $("#inputdiv").fadeOut(500, function() {
-          $("#inputdiv").hide(() => {
-            $("#app").css("font-size", "1.5em");
-            $("#title").html(`mdbs session`);
-            if (data.name === "?????") {
-              let randomSymbols = "!@#$%^&*r";
-              randomSymbols = randomSymbols.split("");
-              let finalString = "";
-              for (var i = 0; i < 5; i++) {
-                let currentSymbol =
-                  randomSymbols[
-                    Math.floor(Math.random() * randomSymbols.length)
-                  ];
-                if (currentSymbol === "r") {
-                  finalString += "Giles".split("")[i];
-                } else {
-                  finalString += currentSymbol;
+      "login", parseInt(document.forms["idinput"]["id"].value),
+      function(result, data, status) {
+        if (result) {
+          console.log("passed.");
+          $("#inputdiv").fadeOut(500, function() {
+            $("#inputdiv").hide(() => {
+              $("#app").css("font-size", "1.5em");
+              $("#title").html(`mdbs session`);
+              if (data.name === "?????") {
+                let randomSymbols = "!@#$%^&*r";
+                randomSymbols = randomSymbols.split("");
+                let finalString = "";
+                for (var i = 0; i < 5; i++) {
+                  let currentSymbol = randomSymbols[Math.floor(
+                      Math.random() * randomSymbols.length)];
+                  if (currentSymbol === "r") {
+                    finalString += "Giles".split("")[i];
+                  } else {
+                    finalString += currentSymbol;
+                  }
                 }
+                $("#app").html(
+                    `Welcome back to the MDBS, <span class="glitch" style="font-size: 1em" data-text="${
+                        finalString}">${finalString}.</span>`);
+              } else if (data.level > 1) {
+                $("#app").html(`Welcome back to the MDBS, ${data.name}.`);
+              } else {
+                $("#app").html(`Welcome to the MDBS, ${data.name}.`);
               }
-              $("#app").html(
-                `Welcome back to the MDBS, <span class="glitch" style="font-size: 1em" data-text="${finalString}">${finalString}.</span>`
-              );
-            } else if (data.level > 1) {
-              $("#app").html(`Welcome back to the MDBS, ${data.name}.`);
-            } else {
-              $("#app").html(`Welcome to the MDBS, ${data.name}.`);
-            }
+            });
+            $("#app").fadeIn(1000);
+            $("#app").fadeOut(1000, () => {
+              $("#title").html("&#65279;");
+              loadStatus(status, data);
+            });
+            $("#app").fadeIn(1000);
           });
-          $("#app").fadeIn(1000);
-          $("#app").fadeOut(1000, () => {
-            $("#title").html("&#65279;");
-            loadStatus(status, data);
-          });
-          $("#app").fadeIn(1000);
-        });
-      } else {
-        $("#inputdiv").fadeOut(100, () => {
-          document.forms["idinput"]["id"].value = "";
-        });
-        console.log("failed.");
-        $("#inputdiv").fadeIn(100);
-      }
-    }
-  );
+        } else {
+          $("#inputdiv")
+              .fadeOut(100,
+                       () => { document.forms["idinput"]["id"].value = ""; });
+          console.log("failed.");
+          $("#inputdiv").fadeIn(100);
+        }
+      });
 });
