6e8b2b82bed061353d1afe975c08cf14203e634b
rglyxr@gmail.com <Kaden Nguyen>
2020-10-19 00:36:16 -0500

    added some stuff

---
+++ public/script.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyB2pbYVVcpqTwSKQ-Wr0Ds5WWe0EZ1gx7U",
+	authDomain: "ddllhb.firebaseapp.com",
+	databaseURL: "https://ddllhb.firebaseio.com",
+	projectId: "ddllhb",
+	storageBucket: "ddllhb.appspot.com",
+	messagingSenderId: "702462043930",
+	appId: "1:702462043930:web:1db5e64613d72095816ea3",
+	measurementId: "G-S0FXP2QG84",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var socket = io();
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+});
+
+http.listen(3000, () => {
+	console.log("listening on *:3000");
+});
